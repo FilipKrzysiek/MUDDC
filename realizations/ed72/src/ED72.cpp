6a3f5383192b20b9f1399cac60d503ba6f807a21
Krzysztof Swałdek <krzysiek.swal@gmail.com>
2026-01-24 17:52:44 +0100

    Add and update some configuration

--- realizations/ed72/src/ED72.cpp
+++ realizations/ed72/src/ED72.cpp
@@ -5,7 +5,7 @@
 #include "ED72.h"
 #include "MUDDC/BaseDevicesAndEndpoint.h"
 
-ED72::ED72(const bde::I2cDevice& expanderI2cline, const bde::I2cDevice& communicationI2CLine) : BaseController(
+ED72::ED72(const bde::I2cDevice &expanderI2cline, const bde::I2cDevice &communicationI2CLine) : BaseController(
     expanderI2cline, communicationI2CLine) {
     configureMasterDevice();
     configureExpanders();
@@ -25,37 +25,127 @@ void ED72::postTransmissionTask() {
 }
 
 void ED72::postReceiveAndReadDevTask() {
+    simulateBatteryCurrent();
+    masterController();
+    directionController();
+}
+
+void ED72::configureMasterDevice() {
+    // addMasterDeviceGpioInput(4, *DatOutSw::UnlockOverRelay);
+    // addMasterDeviceGpioInput(5, *DatOutSw::LeftDoorIndividualOpen);
+    // addMasterDeviceGpioInput(6, *DatOutSw::LeftDoorCentralOpen);
+    // addMasterDeviceGpioInput(7, *DatOutSw::LeftDoorCentralClose);
+    // addMasterDeviceGpioInput(8, *DatOutSw::DoorBellOpen);
+    // addMasterDeviceGpioInput(9, *DatOutSw::RightDoorCentralClose);
+    // addMasterDeviceGpioInput(10, *DatOutSw::RightDoorCentralOpen);
+    // addMasterDeviceGpioInput(11, *DatOutSw::RightDoorIndividualOpen);
+    // addMasterDeviceGpioInput(2, *DatOutSw::TransformerOffAndUnlocked);
+    // addMasterDeviceGpioInput(3, *DatOutSw::TransformerOn);
+    // addMasterDeviceGpioInput(16, *DatOutSw::ClearShpWatchman);
+    // addMasterDeviceGpioInput(17, *DatOutSw::RadioTelephone);
+    addMasterDeviceGpioInput(22, *DatOutSw::DirCntr_Wk6);
+
+    addMasterDevicePwm(18, datagramIn.hvCurrent1(), {});
+    addMasterDevicePwm(19, datagramIn.hvVoltage(), {});
+    addMasterDevicePwm(20, VarPtrProxy<uint16_t>(reinterpret_cast<uint8_t *>(&lvCurrent)), {});
+    addMasterDevicePwm(21, datagramIn.lvVoltage(), {});
+}
+
+void ED72::configureExpanders() {
+    rp1 = bde::PiPicoExpanderEp(0x20, 0b1111'1111'1111'1111'1111'1111, 0, {
+                                    *DatOutSw::LeftLamp,
+                                    *DatOutSw::RightLamp,
+                                    *DatOutSw::TopLamp,
+                                    *DatOutSw::LeftRedLamp,
+                                    *DatOutSw::RightRedLamp,
+                                    *DatOutSw::BoardLighting,
+                                    *DatOutSw::CabinLightingI,
+                                    *DatOutSw::CabinLightingIi,
+                                },
+                                bde::CommunicationDir::Read);
+
+    ex2 = bde::ExpanderEp_8(0x44, 0b1111'1111, 0b0000'0000,
+                            {
+                                *DatOutSw::MasterCtrl_P, *DatOutSw::MasterCtrl_S, *DatOutSw::MasterCtrl_R,
+                                *DatOutSw::MasterCtrl_B1, *DatOutSw::MasterCtrl_B2, *DatOutSw::MasterCtrl_B3,
+                                *DatOutSw::DirCntr_Wk7, *DatOutSw::DirCntr_Wk7
+                            },
+                            bde::CommunicationDir::Read);
+}
+
+void ED72::masterController() {
+    bool pozP = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_P);
+    bool pozS = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_S);
+    bool pozR = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_R);
+    bool pozB1 = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_B1);
+    bool pozB2 = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_B2);
+    bool pozB3 = ex2.getValueOnDataBit(*DatOutSw::MasterCtrl_B3);
+
+    uint8_t checkSum = pozP + pozS + pozR + pozB1 + pozB2 + pozB3;
+
+    if (checkSum == 0) {
+        datagramOut.setMasterController(0);
+    } else if (checkSum == 1) {
+        if (pozP) {
+            datagramOut.setMasterController(42);
+        } else if (pozS) {
+            datagramOut.setMasterController(85);
+        } else if (pozR) {
+            datagramOut.setMasterController(127);
+        } else if (pozB1) {
+            datagramOut.setMasterController(170);
+        } else if (pozB2) {
+            datagramOut.setMasterController(212);
+        } else {
+            datagramOut.setMasterController(255);
+        }
+    } else {
+        datagramOut.setMasterController(0);
+    }
+}
+
+void ED72::directionController() {
+    auto wk6 = std::ranges::find_if(masterDeviceInputs, [](const bde::GpioEp &input) {
+        return input.bit == *DatOutSw::DirCntr_Wk6;
+    })->value;
+
+    auto wk7 = std::ranges::find_if(masterDeviceInputs, [](const bde::GpioEp &input) {
+        return input.bit == *DatOutSw::DirCntr_Wk7;
+    })->value;
+
+    auto wk9 = std::ranges::find_if(masterDeviceInputs, [](const bde::GpioEp &input) {
+        return input.bit == *DatOutSw::DirCntr_Wk9;
+    })->value;
+
+    if (!wk6 && wk7 && wk9) {
+        datagramOut.setSwitchState(*DatOutSw::ReverserBackward, true);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForwardI, false);
+    } else if (!wk6 && !wk7 && wk9) {
+        // Pos off
+        datagramOut.setSwitchState(*DatOutSw::ReverserBackward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForwardI, false);
+    } else if (wk6 && wk7 && wk9) {
+        datagramOut.setSwitchState(*DatOutSw::ReverserBackward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForward, true);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForwardI, false);
+    } else if (wk6 && !wk7 && wk9) {
+        datagramOut.setSwitchState(*DatOutSw::ReverserBackward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForwardI, true);
+    } else {
+        // Pos 0
+        datagramOut.setSwitchState(*DatOutSw::ReverserBackward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForward, false);
+        datagramOut.setSwitchState(*DatOutSw::ReverserForwardI, false);
+    }
+}
+
+void ED72::simulateBatteryCurrent() {
     if (datagramIn.indicatorState(DatagramIn::Battery)) {
         lvCurrent = bde::PwmEp::fromPercentage(50);
     } else {
         lvCurrent = 0;
     }
-}
-
-void ED72::configureMasterDevice() {
-    addMasterDeviceGpioInput(4, *DatOutSw::UnlockOverRelay);
-    addMasterDeviceGpioInput(5, *DatOutSw::LeftDoorIndividualOpen);
-    addMasterDeviceGpioInput(6, *DatOutSw::LeftDoorCentralOpen);
-    addMasterDeviceGpioInput(7, *DatOutSw::LeftDoorCentralClose);
-    addMasterDeviceGpioInput(8, *DatOutSw::DoorBellOpen);
-    addMasterDeviceGpioInput(9, *DatOutSw::RightDoorCentralClose);
-    addMasterDeviceGpioInput(10, *DatOutSw::RightDoorCentralOpen);
-    addMasterDeviceGpioInput(11, *DatOutSw::RightDoorIndividualOpen);
-    addMasterDeviceGpioInput(2, *DatOutSw::TransformerOffAndUnlocked);
-    addMasterDeviceGpioInput(3, *DatOutSw::TransformerOn);
-    addMasterDeviceGpioInput(16, *DatOutSw::ClearShpWatchman);
-    addMasterDeviceGpioInput(17, *DatOutSw::RadioTelephone);
-    addMasterDeviceGpioInput(22, *DatOutSw::DirCntr_Wk6);
-
-    addMasterDevicePwm(18, datagramIn.hvCurrent1(), {});
-    addMasterDevicePwm(19, datagramIn.hvVoltage(), {});
-    addMasterDevicePwm(20, VarPtrProxy<uint16_t>(reinterpret_cast<uint8_t*>(&lvCurrent)), {});
-    addMasterDevicePwm(21, datagramIn.lvVoltage(), {});
-}
-
-void ED72::configureExpanders() {
-    // rp1 = bde::PiPicoExpanderEp(0x48);
-    ex2 = bde::ExpanderEp_8(0x44, 0b1111'1111, 0b0000'0000,
-                            {-1, -1, -1, -1, -1, -1, bde::u(DatOutSw::DirCntr_Wk7), bde::u(DatOutSw::DirCntr_Wk7)},
-                            bde::CommunicationDir::Read);
 }
--- realizations/ed72/src/ED72.h
+++ realizations/ed72/src/ED72.h
@@ -16,22 +16,46 @@ public:
     void postReceiveAndReadDevTask() override;
 
     enum class DatOutSw: bde::datBit_t {
+        MasterCtrl_B3 = -10,
+        MasterCtrl_B2 = -9,
+        MasterCtrl_B1 = -8,
+        MasterCtrl_R = -7,
+        MasterCtrl_S = -6,
+        MasterCtrl_P = -5,
         DirCntr_Wk9 = -4,
         DirCntr_Wk7 = -3,
         DirCntr_Wk6 = -2,
         Undefined = -1,
-        UnlockOverRelay = 0,
-        LeftDoorIndividualOpen = 1,
-        LeftDoorCentralOpen = 2,
-        LeftDoorCentralClose = 3,
-        DoorBellOpen = 4,
-        RightDoorCentralOpen = 5,
-        RightDoorCentralClose = 6,
-        RightDoorIndividualOpen = 7,
-        TransformerOffAndUnlocked = 8,
-        TransformerOn = 9,
-        ClearShpWatchman = 10,
-        RadioTelephone = 11,
+        LeftLamp = 0,
+        RightLamp = 1,
+        TopLamp = 2,
+        LeftRedLamp = 3,
+        RightRedLamp = 4,
+        BoardLighting = 5,
+        CabinLightingI = 6,
+        CabinLightingIi = 7,
+        EnabledInteriorLighting = 8,
+        DisabledInteriorLighting = 9,
+        AllPantographsLoweredDown = 10,
+        Compressor = 11,
+        UnlockCompressor = 12,
+        Buzzer = 13,
+        ReverserBackward = 14,
+        ReverserForward = 15,
+        ReverserForwardI = 16,
+
+        // UnlockOverRelay = 0,
+        // LeftDoorIndividualOpen = 1,
+        // LeftDoorCentralOpen = 2,
+        // LeftDoorCentralClose = 3,
+        // DoorBellOpen = 4,
+        // RightDoorCentralOpen = 5,
+        // RightDoorCentralClose = 6,
+        // RightDoorIndividualOpen = 7,
+        // TransformerOffAndUnlocked = 8,
+        // TransformerOn = 9,
+        // ClearShpWatchman = 10,
+        // RadioTelephone = 11,
     };
 
 private:
@@ -44,6 +68,11 @@ private:
 
     void configureMasterDevice();
     void configureExpanders();
+
+    void masterController();
+    void directionController();
+
+    void simulateBatteryCurrent();
 };
 
 constexpr bde::datBit_t operator*(ED72::DatOutSw enumValue) {
